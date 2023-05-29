ac26e8b5e93ae276121c793bb22b1e7608dd9fed
ArthurEduardoFary <arthurfary22@gmail.com>
2023-05-29 09:29:58 -0300

    Initial commit

---
+++ components/carousel.js
@@ -0,0 +1,32 @@
+'use client'
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+import "./carousel_styles.css";
+
+// import required modules
+import { Navigation } from "swiper";
+
+export default function App() {
+  return (
+    <>
+      <Swiper navigation={true} modules={[Navigation]} className="h-screen mt-20">
+        <SwiperSlide><img src="/images/tea.png" alt=""/></SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
